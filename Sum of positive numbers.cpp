e12e3a42367bfd9e435ded28744cc35695259f9f
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-09-05 09:38:56 +0530

    Add files via upload

---
+++ Sum of positive numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int num1, num2,sum;
+cout<<"Enter the integer 1\n";
+cin>>num1;
+cout<<"Enter the integer 2\n";
+cin>>num2;
+sum = num1 + num2;
+printf("%d", sum);
+
+return 0;
+}
