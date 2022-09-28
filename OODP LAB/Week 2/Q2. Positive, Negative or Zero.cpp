c50d222533e692bd8bc28d05837ccaaca0d553e9
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-09-28 23:24:31 +0530

    Create Q2. Positive, Negative or Zero.cpp

---
+++ OODP LAB/Week 2/Q2. Positive, Negative or Zero.cpp
@@ -0,0 +1,28 @@
+/* Write a C++ program to print whether the given number is positive number or negative number. */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int num;
+  
+    cout<<"Enter the number\n";
+    cin>>num;
+
+    if(num>0)
+    {
+        cout<<"The given number "<<num<<" is a positive number";
+    }
+    if (num<0)
+    {
+        cout<<"The given number "<<num<<" is a negative number";
+    }
+    if (num==0)
+    {
+        cout<<"The number zero is neither positive nor negative.";
+    }
+
+return 0;
+}
