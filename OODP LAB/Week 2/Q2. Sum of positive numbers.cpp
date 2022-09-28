fec3d47b0b62cbf793344cd7e8d6372b47f9d2bc
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-09-28 22:48:24 +0530

    Update Q2. Sum of positive numbers.cpp

--- OODP LAB/Week 2/Q2. Sum of positive numbers.cpp
+++ OODP LAB/Week 2/Q2. Sum of positive numbers.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 
-int main(){
-int num1, num2,sum;
-cout<<"Enter the integer 1\n";
-cin>>num1;
-cout<<"Enter the integer 2\n";
-cin>>num2;
-sum = num1 + num2;
-printf("%d", sum);
+int main()
+{
+  int num1, num2,sum;
+  
+  cout<<"Enter the integer 1\n";
+  cin>>num1;
+
+  cout<<"Enter the integer 2\n";
+  cin>>num2;
+
+  sum = num1 + num2;
+  
+  printf("%d", sum);
 
 return 0;
+  
 }
