914333bf623096e0e5d5f92986bb317540125c52
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-09-30 00:17:31 +0530

    Create Q3.cpp

---
+++ OODP LAB/Week 5/Q3.cpp
@@ -0,0 +1,35 @@
+/* Create a class to print the area of a square and a rectangle. The class has two
+functions with the same name but different number of parameters. The function for
+printing the area of rectangle has two parameters which are its length and breadth
+respectively while the other function for printing the area of square has one
+parameter which is the side of the square. */
+#include <iostream>
+
+using namespace std;
+
+class Area
+{
+  public:
+    void print_area(int length, int breadth)
+    {
+        cout <<"Length : " << length<<endl;
+        cout <<"Width : " << breadth<<endl;
+        cout <<"Area of the rectangle is : "<<length*breadth << endl;
+    }
+
+    void print_area(int length)
+    {
+        cout <<"Side : " << length<<endl;
+        cout <<"Area of the square is : "<<length*length << endl;
+    }
+};
+
+int main()
+{
+    Area rectangle, square;
+  
+    square.print_area(7);
+    rectangle.print_area(7,8);
+  
+    return 0;
+}
