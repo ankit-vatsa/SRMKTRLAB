2c48e3ac171cc67a26f7603b960edb8e04db1ddc
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-09-30 08:27:03 +0530

    Update Q1.cpp

--- OODP LAB/Week 5/Q1.cpp
+++ OODP LAB/Week 5/Q1.cpp
@@ -2,6 +2,7 @@
 name is passed while creating an object of the Student class, then the name should
 be "Unknown", otherwise the name should be equal to the String value passed while
 creating the object of the Student class. */
+
 #include <iostream>
 
 using namespace std;
