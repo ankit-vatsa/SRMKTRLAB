4b889d8c38bc1cc4f13d7837d89175939b80791e
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-09-30 23:40:25 +0530

    Create Week 1_Q2_class.cpp

---
+++ OODP LAB/Week 1/Week 1_Q2_class.cpp
@@ -0,0 +1,43 @@
+//Program Description 
+// Write a C++ program to find those numbers which are divisible by 8 and multiple of 5, between 1000 and 2000 (both included)
+
+#include <iostream>
+
+using namespace std;
+
+class Calculator
+{
+    int num1,num2;
+public:
+  void getDetails()
+  {
+      cout<<"Enter number 1"<<endl;
+      cin>>num1;
+      
+      cout<<"Enter number "<<endl;
+      cin>>num2;
+  }
+  void divnums();
+};
+
+int main()
+{
+Calculator object;
+
+object.getDetails();
+object.divnums();
+
+return 0;
+}
+
+void Calculator::divnums()
+{ 
+    cout<<"Numbers are : "<<endl;
+      for(int i = num1; i<=num2; i++)
+      { 
+        if(i%8==0&&i%5==0) 
+        { 
+            cout<<i<<endl;
+        }
+      }
+}
