003604470b49a511f416da7eb35ed662d63e16dc
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2022-10-21 23:54:32 +0530

    Create Q9.cpp

---
+++ OODP LAB/Week 7/Q9.cpp
@@ -0,0 +1,50 @@
+/* Write a C++ Program to Access Protected Data Member using Inheritance. */
+#include <iostream>
+using namespace std;
+
+class Maths
+{   protected:
+    int num; //Protected data member
+    
+    public:
+    void getdetails()
+    {
+        cout<<"Enter a number : "<<endl;
+        cin>>num;
+    }
+    void printdetails()
+    {
+        cout<<"Entered number is : "<<endl<<num<<endl;
+    }
+};
+
+class Square:public Maths
+{
+    public:
+    int squareofnum()
+    {
+        return num*num;    //Accessing protected data member of the Maths Class using the Multilevel Inheritance.
+    }
+    void sqprint_details()
+    {
+        cout<<"Square of the number is : "<<endl<<squareofnum()<<endl;
+    }
+};
+
+int main() 
+{
+    Cube number;
+    number.getdetails();
+    number.sqprint_details();
+
+    return 0;
+}
+
+/*
+    Sample Input:
+      Enter a number : 
+      5
+    Sample Output:
+      Square of the number is : 
+      25
+*/
