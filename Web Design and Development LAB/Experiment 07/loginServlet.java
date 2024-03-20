57e42824304a0ac77561827dde5c3c746a07903e
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2024-03-20 09:38:28 +0530

    Update loginServlet.java

--- Web Design and Development LAB/Experiment 07/loginServlet.java
+++ Web Design and Development LAB/Experiment 07/loginServlet.java
@@ -7,19 +7,17 @@ import java.io.PrintWriter;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-
 public class loginServlet extends HttpServlet {
-	
 @Override
  protected void doGet(HttpServletRequest request, HttpServletResponse response)
  	throws ServletException, IOException
  {
 	response.setContentType("text/html");
-	String bob = request.getParameter("babu");
+	String bob = request.getParameter("mera_colour");
 	PrintWriter obj = response.getWriter();
-	String col = request.getParameter("bab");
-	obj.println("<font color="+col+">");
-	obj.println("<h1>Welcome "+bob+"</h1>");
+	String col = request.getParameter("mera_naam");
+	obj.println("<font color="+bob+">");
+	obj.println("<h1>Welcome "+col+"</h1>");
 	obj.println("</font>");
 	obj.close();
  }
