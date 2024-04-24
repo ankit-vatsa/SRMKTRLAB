a0b38413d5cbdddb8f7941bd31a973a7a564a350
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2024-04-25 01:33:13 +0530

    Create ankitService.java

---
+++ Web Design and Development LAB/Experiment11/ankitService.java
@@ -0,0 +1,14 @@
+package com.example.ankit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class ankitService {
+
+	public Integer getData(Integer a, Integer b)
+{
+Integer c=a+b;
+return c;
+}
+}
