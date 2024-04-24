d43c840fa0132bf000056b04b4aad141359aaad8
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2024-04-25 02:03:54 +0530

    Create studentService.java

---
+++ Web Design and Development LAB/Experiment11/P2/studentService.java
@@ -0,0 +1,9 @@
+package com.example.student.service;
+import org.springframework.stereotype.Service;
+@Service
+public class studentService {
+public String getStudent(String name, Integer age)
+{
+return name+",your age is"+ age;
+}
+}
