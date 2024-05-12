7edb8479af1318d9ed3c44495f5941e8f2d73d5e
Ankit Vatsa <103623899+ankit-vatsa@users.noreply.github.com>
2024-05-13 02:47:17 +0530

    Create 003.Latin Square.cpp

---
+++ AI with application in Cloud Lab/003.Latin Square.cpp
@@ -0,0 +1,45 @@
+// C++ program to print Latin Square
+// A Latin Square is a n x n grid filled by n distinct numbers each appearing exactly once in each row and column.
+
+#include <iostream> 
+using namespace std; 
+
+// Function to print n x n Latin Square 
+void printLatin(int n) 
+{ 
+	// A variable to control the rotation 
+	// point. 
+	int k = n+1; 
+
+	// Loop to print rows 
+	for (int i=1; i<=n; i++) 
+	{ 
+		// This loops runs only after first 
+		// iteration of outer loop. It prints 
+		// numbers from n to k 
+		int temp = k; 
+		while (temp <= n) 
+		{ 
+			cout << temp << " "; 
+			temp++; 
+		} 
+
+		// This loop prints numbers from 1 to k-1. 
+		for (int j=1; j<k; j++) 
+			cout << j << " "; 
+
+		k--; 
+		cout << endl; 
+	} 
+} 
+
+// Driver program to test above function 
+int main(void) 
+{ 
+	int n = 5; 
+
+	// Invoking printLatin function 
+	printLatin(n); 
+
+	return 0; 
+} 
